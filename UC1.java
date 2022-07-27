608c1cbdec1a80cfb9efeba1d12a711d86b6c893
PremKumbhar <kumbharprem7738@gmail.com>
2022-07-27 08:52:03 +0530

    [Prem]:added UC1 file

---
+++ UC1.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.TicTacToe;
+
+public class UC1 {
+
+	public static void main(String[] args) {
+		char[][] board =new char [3][3];
+		for (int i=0;i<3;i++)
+		{
+			for (int j=0;j<3;j++)
+			{
+				board[i][j] = ' ';
+			}
+		}
+		
+		
+
+	}
+
+}
