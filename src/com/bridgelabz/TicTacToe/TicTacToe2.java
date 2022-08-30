976cdd9b5531b7cd8a1cdb1cfebb8bb37325556d
PremKumbhar <kumbharprem7738@gmail.com>
2022-08-30 14:12:51 +0530

    [Prem]:Added to display current boared file

---
+++ src/com/bridgelabz/TicTacToe/TicTacToe2.java
@@ -0,0 +1,45 @@
+package com.bridgelabz.TicTacToe;
+
+import java.util.Scanner;
+
+public class TicTacToe2 
+{
+	
+	
+	  static String[] board;
+	    static String turn;
+	    
+	    static void printBoard()
+	    {
+	        System.out.println("|---|---|---|");
+	        System.out.println("| " + board[0] + " | "
+	                           + board[1] + " | " + board[2]
+	                           + " |");
+	        System.out.println("|-----------|");
+	        System.out.println("| " + board[3] + " | "
+	                           + board[4] + " | " + board[5]
+	                           + " |");
+	        System.out.println("|-----------|");
+	        System.out.println("| " + board[6] + " | "
+	                           + board[7] + " | " + board[8]
+	                           + " |");
+	        System.out.println("|---|---|---|");
+	    }
+	  
+	    public static void main(String[] args)
+	    {
+	        Scanner in = new Scanner(System.in);
+	        board = new String[9];
+	        turn = "X";
+	        String winner = null;
+	  
+	        for (int a = 0; a < 9; a++) {
+	            board[a] = String.valueOf(a + 1);
+	        }
+	        System.out.println("*****Welcome to Workshop platform*****");
+
+	  
+	        System.out.println(" Welcome to Tic Tac Toe Game ");
+	        printBoard();
+	    }
+	    }
